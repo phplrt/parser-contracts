83959a3719ef1908eddd6c4afe319fc37f6c20d7
SerafimArts <nesk@xakep.ru>
2026-07-29 19:38:23 +0000

    Fix docs

--- src/Exception/RuntimeExceptionInterface.php
+++ src/Exception/RuntimeExceptionInterface.php
@@ -28,14 +28,16 @@ interface RuntimeExceptionInterface extends ParserExceptionInterface
     }
 
     /**
-     * The final offset at which the parser error occurred.
+     * The size of the source fragment the parser error occurred in, in bytes.
      *
-     * If the offset is not specified ({@see null}), the token offset
-     * ({@see TokenInterface::$end}) can be used instead.
+     * A parser fails on a token and may span as far as the rule it has failed
+     * on, which is what this tells apart from the token itself. If the size is
+     * not specified ({@see null}), that of the token ({@see TokenInterface::$size})
+     * can be used instead.
      *
      * @var int<0, max>|null
      */
-    public ?int $end {
+    public ?int $length {
         get;
     }
 }
