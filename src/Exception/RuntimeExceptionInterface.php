70ff61b02bbda2891b6dfd51e155b60433bcbd57
SerafimArts <nesk@xakep.ru>
2026-07-29 13:40:29 +0000

    Actualize docs

--- src/Exception/RuntimeExceptionInterface.php
+++ src/Exception/RuntimeExceptionInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Phplrt\Contracts\Parser\Exception;
 
 use Phplrt\Contracts\Lexer\TokenInterface;
+use Phplrt\Contracts\Source\ReadableInterface;
 
 /**
  * An exception that occurs after starting the parsing and indicates
@@ -13,9 +14,28 @@ use Phplrt\Contracts\Lexer\TokenInterface;
 interface RuntimeExceptionInterface extends ParserExceptionInterface
 {
     /**
+     * Gets the source object in which the error occurred.
+     */
+    public ReadableInterface $source {
+        get;
+    }
+
+    /**
      * Gets the token on which the error occurred.
      */
     public TokenInterface $token {
+        get;
+    }
+
+    /**
+     * The final offset at which the parser error occurred.
+     *
+     * If the offset is not specified ({@see null}), the token offset
+     * ({@see TokenInterface::$end}) can be used instead.
+     *
+     * @var int<0, max>|null
+     */
+    public ?int $end {
         get;
     }
 }
